9c575d7435e996a677b1d313b6653da2d6a30b00
Piyush-088 <piyushpal7607@gmail.com>
2025-03-28 21:47:06 +0530

    Delete Task_2.js

--- Task_2.js
+++
@@ -1,7 +0,0 @@
-function alphabetizeString(str) {
-    return str.split('').sort().join('');
-}
-
-const exampleString = 'webmaster';
-const sortedString = alphabetizeString(exampleString);
-console.log(sortedString);
